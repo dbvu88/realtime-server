f6a56830565b0fe62c0a0341263fbaf5d52fbeee
Duc Vu <de.yd315@gmail.com>
2019-10-06 16:15:39 -0500

    added drawings events handlers

--- index.js
+++ index.js
@@ -3,21 +3,55 @@ const http = require("http").createServer(app);
 const io = require("socket.io")();
 
 const cors = require("cors");
+
+const r = require("rethinkdb");
+const { createDrawing, subscribeToDrawings } = require("./models/drawings");
 app.use(cors());
+
+r.connect({
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 28015,
+  db: process.env.DB_NAME || "awesome_whiteboard"
+}).then(connection => {
+  io.on("connection", client => {
+    // on() - subscribe to events
+    client.on("subscribeToTimer", interval => {
+      console.log("client is subscribing to timer with interval ", interval);
+      // listen to the changes in the timers table
+      r.table("timers")
+        .changes()
+        .run(connection)
+        .then(cursor => {
+          cursor.each((err, timerRow) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            client.emit("timer", timerRow.new_val.timestamp);
+          });
+        });
+
+      //   setInterval(() => {
+      //     // emit() - publish events
+      //     client.emit("timer", new Date());
+      //   }, interval);
+    });
+
+    client.on("createDrawing", ({ name }) =>
+      createDrawing({ connection, name })
+    );
+
+    client.on("subscribeToDrawings", () =>
+      subscribeToDrawings({
+        client,
+        connection
+      })
+    );
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("hello");
-});
-
-io.on("connection", client => {
-  // on() - subscribe to events
-  client.on("subscribeToTimer", interval => {
-    console.log("client is subscribing to timer with interval ", interval);
-    setInterval(() => {
-      // emit() - publish events
-      client.emit("timer", new Date());
-    }, interval);
-  });
 });
 
 const port = process.env.PORT || 8000;
