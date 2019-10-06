e78183e1f6d0870cebab1df989ca0f6660d48505
Duc Vu <de.yd315@gmail.com>
2019-10-06 16:15:05 -0500

    created drawing models

---
+++ models/drawings.js
@@ -0,0 +1,27 @@
+const r = require("rethinkdb");
+
+const createDrawing = ({ connection, name }) => {
+  r.table("drawings")
+    .insert({
+      name,
+      timestamp: new Date()
+    })
+    .run(connection)
+    .then(() => console.log("created a drawing with name: ", name));
+};
+
+const subscribeToDrawings = ({ client, connection }) => {
+  r.table("drawings")
+    .changes({ includeInitial: true })
+    .run(connection)
+    .then(cursor => {
+      cursor.each((err, drawingRow) =>
+        client.emit("drawing", drawingRow.new_val)
+      );
+    });
+};
+
+module.exports = {
+  createDrawing,
+  subscribeToDrawings
+};
